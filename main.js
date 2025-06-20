dafa4169fb176b138522d1eac59846c2b4fe574a
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-06-20 05:37:56 +0300

    Setting up react and tailwindcss in the electron app

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow ,ipcMain} = require('electron')
 const path = require('node:path')
 
-require('update-electron-app')()
 
 
 
@@ -9,12 +8,22 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+     autoHideMenuBar: true, // ✅ This hides the menu bar
+    icon: path.join(__dirname, 'assets/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadFile('index.html')
+//   if (app.isPackaged) {
+//   win.loadFile(path.join(__dirname, 'frontend/dist/index.html'));
+// } else {
+//   win.loadURL('http://localhost:5173');
+// }
+  win.loadURL('http://localhost:5173');
+
+    Menu.setApplicationMenu(null);
+
 }
 
 app.whenReady().then(() => {
