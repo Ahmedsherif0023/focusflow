3b8998a69308ffe2b595f153f950708ca8a129e3
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-06-20 04:42:47 +0300

    added some code

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,19 @@ module.exports = {
       config: {},
     },
   ],
+   publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'Ahmedsherif0023',
+          name: 'focusflow'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
